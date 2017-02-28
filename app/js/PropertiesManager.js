9576b4dffb5f393d3a9296f64a1aa0a7825c99a1
Aleksandrenko <aleksandrenko@gmail.com>
2017-02-28 23:26:19 +0200

    Attach events to properties edit, clean css

--- app/js/PropertiesManager.js
+++ app/js/PropertiesManager.js
@@ -102,7 +102,7 @@ const PM = {
             entity.properties = entity.properties.filter((prop) => prop.id !== property.id);
             _drawProperties(entity);
           } else {
-            _drawPropertyInEdit(entity, property);
+            _drawPropertyInEdit(property);
           }
         };
 
@@ -118,333 +118,49 @@ const PM = {
     /**
      * @private
      */
-    const _drawPropertyInEdit = (entity, property) => {
+    const _drawPropertyInEdit = (property) => {
       const editWrapper = document.querySelector('#property-edit');
       editWrapper.classList.add('open-wrapper');
+
       editWrapper.innerHTML = `
 <ul>
   <li>
-    <select onchange="">
-      ${ Object.keys(PROPERTY_TYPES).map((type) => `<option value="${PROPERTY_TYPES[type]}">${PROPERTY_TYPES[type]}</option>`) }
+    <select>
+      ${ Object.keys(PROPERTY_TYPES).map((type) => `<option ${ property.type.toLowerCase() === PROPERTY_TYPES[type].toLowerCase() ? 'selected="true"' : '' }" value="${PROPERTY_TYPES[type]}">${PROPERTY_TYPES[type]}</option>`) }
     </select>
   </li>
   
-  <li>defaultValue: <b>${property.defaultValue}</b></li>
-  <li>entity.type: <b>${entity.type}</b></li>
-  <li>hasDefaultValue: <b>${property.hasDefaultValue}</b></li>
-  <li>hasLimit: <b>${property.hasLimit}</b></li>
-  <li>isRequired: <b>${property.isRequired}</b></li>
-  <li>limit[0]: <b>${property.limit[0]}</b></li>
-  <li>limit[1]: <b>${property.limit[1]}</b></li>
+  <li>defaultValue: <input class="defaultValue" value="${property.defaultValue}" /></li>
+  <li><label>isRequired: <input class="isRequired" type="checkbox" ${property.isRequired ? 'checked' : ''} /></label></li>
+  <li>min: <input class="limitMin" type="text" value="${property.limit[0]}" /></li>
+  <li>max: <input class="limitMin" type="text" value="${property.limit[1]}" /></li>
 </ul>
 `;
 
-      // const editWrapper = d3.select('.property-edit');
-      //
-      // editWrapper.html('');
-      //
-      // if (!prop) {
-      //   editWrapper.style({display: 'none'});
-      //   return false;
-      // } else {
-      //   editWrapper.style({display: 'inline-block'});
-      // }
-      //
-      // const propertyParamsInEdit = editWrapper.append('ul');
-      //
-      // /** --------------------------
-      //  * property key input
-      //  --------------------------- */
-      //
-      // propertyParamsInEdit
-      //   .append('li')
-      //   .append('input')
-      //   .attr({
-      //     placeholder: 'Key',
-      //     type: 'text',
-      //     value: () => prop.key
-      //   })
-      //   .on('change', () => {
-      //     prop.key = d3.event.target.value;
-      //     _drawPropertyInEdit();
-      //   });
-      //
-      // /** -------------------------------
-      //  * property type drop-down/select
-      //  ------------------------------- */
-      //
-      // const propertyParamsInEditSelect = propertyParamsInEdit
-      //   .append('li')
-      //   .append('select')
-      //   .attr({
-      //     id: 'property-types'
-      //   });
-      //
-      // propertyParamsInEditSelect.on('input', () => {
-      //   prop.type = d3.event.target.value;
-      //
-      //   prop.hasLimit = false;
-      //   prop.limit = [0, 0];
-      //   prop.hasDefaultValue = false;
-      //   prop.defaultValue = '';
-      //
-      //   _drawPropertyInEdit();
-      // });
-      //
-      // propertyParamsInEditSelect
-      //   .selectAll('option')
-      //   .data(Object.keys(PROPERTY_TYPES))
-      //   .enter()
-      //   .append('option')
-      //   .attr({
-      //     value: type => type
-      //   })
-      //   .text(type => PROPERTY_TYPES[type]);
-      //
-      // d3.select(`option[value="${prop.type}"]`).attr({
-      //   selected: true
-      // });
-      //
-      // /** ------------------------------
-      //  * Property has default checkbox
-      //  ------------------------------- */
-      //
-      // if (prop.type) {
-      //   const propHasDefaultCheckbox = propertyParamsInEdit
-      //     .append('li')
-      //     .append('label')
-      //     .text('Has Default Value')
-      //     .append('input')
-      //     .attr({
-      //       type: 'checkbox'
-      //     })
-      //     .on('change', () => {
-      //       prop.hasDefaultValue = d3.event.target.checked;
-      //
-      //       if (!prop.hasDefaultValue) {
-      //         prop.defaultValue = '';
-      //       }
-      //       _drawPropertyInEdit();
-      //     });
-      //
-      //   /** -----------------------------------------
-      //    * property default value input/radio button
-      //    ------------------------------------------ */
-      //
-      //   if (prop.hasDefaultValue) {
-      //     propHasDefaultCheckbox.attr({checked: true});
-      //
-      //     if (prop.type === PROPERTY_TYPES.BOOLEAN) {
-      //       const propertyParamsInEditDefaultBoolean = propertyParamsInEdit.append('li');
-      //
-      //       propertyParamsInEditDefaultBoolean.classed('default-bool', true);
-      //
-      //       const defaultPropertyTruth = propertyParamsInEditDefaultBoolean
-      //         .append('label')
-      //         .text('True')
-      //         .append('input')
-      //         .attr({
-      //           type: 'radio',
-      //           name: 'defaultBool'
-      //         });
-      //
-      //       if (prop.defaultValue === true) {
-      //         defaultPropertyTruth.attr({checked: true});
-      //       }
-      //
-      //       defaultPropertyTruth.on('click', () => {
-      //         prop.defaultValue = true;
-      //         _drawPropertyInEdit();
-      //       });
-      //
-      //       const defaultPropertyFalse = propertyParamsInEditDefaultBoolean
-      //         .append('label')
-      //         .text('False')
-      //         .append('input')
-      //         .attr({
-      //           type: 'radio',
-      //           name: 'defaultBool'
-      //         });
-      //
-      //       if (prop.defaultValue === false) {
-      //         defaultPropertyFalse.attr({checked: true});
-      //       }
-      //
-      //       defaultPropertyFalse.on('click', () => {
-      //         prop.defaultValue = false;
-      //         _drawPropertyInEdit();
-      //       });
-      //     } else {
-      //       const isDate = prop.type.toLowerCase() === PROPERTY_TYPES.DATE.toLowerCase();
-      //       let dateDefaultValueNowCheckbox;
-      //
-      //       if (isDate) {
-      //         dateDefaultValueNowCheckbox = propertyParamsInEdit
-      //           .append('li')
-      //           .append('label')
-      //           .text('Default is "now"')
-      //           .append('input')
-      //           .attr({
-      //             type: 'checkbox'
-      //           })
-      //           .on('change', () => {
-      //             if (d3.event.target.checked) {
-      //               prop.defaultValue = 'now';
-      //             } else {
-      //               prop.defaultValue = '';
-      //             }
-      //
-      //             _drawPropertyInEdit();
-      //           });
-      //       }
-      //
-      //       const defaultValueInput = propertyParamsInEdit
-      //         .append('li')
-      //         .append('input')
-      //         .attr({
-      //           placeholder: 'Default Value',
-      //           type: isDate ? 'date' : 'text',
-      //           value: () => prop.defaultValue
-      //         })
-      //         .style({
-      //           display: () => prop.hasDefaultValue ? 'inherit' : 'none'
-      //         })
-      //         .on('change', () => {
-      //           prop.defaultValue = d3.event.target.value;
-      //           _drawPropertyInEdit();
-      //         });
-      //
-      //       if (prop.defaultValue === 'now' && isDate) {
-      //         defaultValueInput.attr('disabled', true);
-      //         dateDefaultValueNowCheckbox.attr('checked', true);
-      //       }
-      //     }
-      //   }
-      // }
-      //
-      // /** --------------------------
-      //  * property limit checkbox
-      //  --------------------------- */
-      //
-      // if (prop.type === PROPERTY_TYPES.STRING ||
-      //   prop.type === PROPERTY_TYPES.PASSWORD ||
-      //   prop.type === PROPERTY_TYPES.EMAIL ||
-      //   prop.type === PROPERTY_TYPES.URL ||
-      //   prop.type === PROPERTY_TYPES.LATLNG ||
-      //   prop.type === PROPERTY_TYPES.FLOAT ||
-      //   prop.type === PROPERTY_TYPES.INT) {
-      //
-      //   const propHasLimitCheckbox = propertyParamsInEdit
-      //     .append('li')
-      //     .append('label')
-      //     .text('Has Limit')
-      //     .append('input')
-      //     .attr({
-      //       type: 'checkbox'
-      //     })
-      //     .on('change', () => {
-      //       prop.hasLimit = d3.event.target.checked;
-      //
-      //       if (!prop.hasLimit) {
-      //         prop.limit = [0, 0];
-      //       }
-      //       _drawPropertyInEdit();
-      //     });
-      //
-      //   if (prop.hasLimit) {
-      //     propHasLimitCheckbox.attr({checked: true});
-      //
-      //     const propertyParamsInEditLimits = propertyParamsInEdit.append('li').classed('hbox', true);
-      //
-      //     /** --------------------------------
-      //      * property limit inputs for number
-      //      --------------------------------- */
-      //
-      //     if (prop.type === PROPERTY_TYPES.INT && prop.type === PROPERTY_TYPES.FLOAT) {
-      //       // if it is of type number
-      //       propertyParamsInEditLimits
-      //         .append('input')
-      //         .attr({
-      //           placeholder: 'Min',
-      //           type: 'number',
-      //           value: () => prop.limit[0]
-      //         })
-      //         .on('change', () => {
-      //           prop.limit[0] = d3.event.target.value;
-      //           _drawPropertyInEdit();
-      //         });
-      //
-      //       propertyParamsInEditLimits
-      //         .append('input')
-      //         .attr({
-      //           placeholder: 'Max',
-      //           type: 'number',
-      //           value: () => prop.limit[1]
-      //         })
-      //         .on('change', () => {
-      //           prop.limit[1] = d3.event.target.value;
-      //           _drawPropertyInEdit();
-      //         });
-      //     }
-      //
-      //     /** ---------------------------------
-      //      * property limit inputs for strings
-      //      ---------------------------------- */
-      //
-      //     if (prop.type === PROPERTY_TYPES.STRING ||
-      //       prop.type === PROPERTY_TYPES.PASSWORD ||
-      //       prop.type === PROPERTY_TYPES.EMAIL ||
-      //       prop.type === PROPERTY_TYPES.URL ||
-      //       prop.type === PROPERTY_TYPES.LATLNG) {
-      //
-      //       propertyParamsInEditLimits
-      //         .append('input')
-      //         .attr({
-      //           placeholder: 'Min',
-      //           type: 'number',
-      //           min: 0,
-      //           value: () => prop.limit[0]
-      //         })
-      //         .on('change', () => {
-      //           prop.limit[0] = d3.event.target.value;
-      //           _drawPropertyInEdit();
-      //         });
-      //
-      //       propertyParamsInEditLimits
-      //         .append('input')
-      //         .attr({
-      //           placeholder: 'Max',
-      //           type: 'number',
-      //           value: () => prop.limit[1]
-      //         })
-      //         .on('change', () => {
-      //           prop.limit[1] = d3.event.target.value;
-      //           _drawPropertyInEdit();
-      //         });
-      //     }
-      //   }
-      // }
-      //
-      // /** -----------------------------
-      //  * property is required checkbox
-      //  ------------------------------ */
-      //
-      // const propIsRequiredCheckbox = propertyParamsInEdit
-      //   .append('li')
-      //   .append('label')
-      //   .text('Is Required')
-      //   .append('input')
-      //   .attr({
-      //     type: 'checkbox'
-      //   })
-      //   .on('change', () => {
-      //     prop.isRequired = d3.event.target.checked;
-      //     _drawPropertyInEdit();
-      //   });
-      //
-      // if (prop.isRequired) {
-      //   propIsRequiredCheckbox.attr({checked: true});
-      // }
+      d3.select('#property-edit select').on('change', () => {
+        console.log('select change', d3.event.target.value);
+        property.type = d3.event.target.value;
+      });
+
+      d3.select('#property-edit .defaultValue').on('input', () => {
+        console.log('defaultValue input', d3.event.target.value);
+        property.defaultValue = d3.event.target.value;
+      });
+
+      d3.select('#property-edit .isRequired').on('click', () => {
+        console.log('isRequired click', d3.event.target.value);
+        property.isRequired = d3.event.target.value;
+      });
+
+      d3.select('#property-edit .limitMin').on('input', () => {
+        console.log('limitMin input', d3.event.target.value);
+        property.limit[0] = d3.event.target.value;
+      });
+
+      d3.select('#property-edit .limitMin').on('input', () => {
+        console.log('limitMin input', d3.event.target.value);
+        property.limit[1] = d3.event.target.value;
+      });
     };
 
     /** --------------------------
--- app/js/do/Property.js
+++ app/js/do/Property.js
@@ -10,9 +10,7 @@ class Property {
     this.id = options.id || createId();
     this.key = (options.key || 'New Property').toLowerCase();
     this.type = (options.type || '').toLowerCase();
-    this.hasDefaultValue = options.hasDefaultValue || false;
     this.defaultValue = options.defaultValue || '';
-    this.hasLimit = options.hasLimit || false;
     this.limit = options.limit || ['', ''];
     this.isRequired = options.isRequired || false;
   }
