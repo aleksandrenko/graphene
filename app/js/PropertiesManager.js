edb4fd013a4e472821467249e327fed12a132c9e
Nikolai Aleksandrenko <aleksandrenko@gmail.com>
2017-03-02 14:35:41 +0200

    Adding preact with sample component to start from (unstable)

--- app/js/PropertiesManager.js
+++ app/js/PropertiesManager.js
@@ -1,9 +1,43 @@
 'use strict';
+/** @jsx h */
 
 import CONST from './enums/CONST';
 import PROPERTY_TYPES from './enums/PROPERTY_TYPES';
 import Property from './do/Property';
 import createDomElementInContainer from './utils/dom';
+
+
+import { h, render, Component } from 'preact';
+
+/* example component to start from */
+class Clock extends Component {
+  constructor() {
+    super();
+    // set initial time:
+    this.state.time = Date.now();
+  }
+
+  componentDidMount() {
+    // update time every second
+    this.timer = setInterval(() => {
+      this.setState({ time: Date.now() });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    // stop when not renderable
+    clearInterval(this.timer);
+  }
+
+  render(props, state) {
+    let time = new Date(state.time).toLocaleTimeString();
+    return <span>{ time }</span>;
+  }
+}
+
+// render an instance of Clock into <body>:
+render(<Clock />, document.body);
+
 
 let _entity;
 let _saveHandlerFunction = () => null;
